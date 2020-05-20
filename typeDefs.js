36e948611e5e005b65c6d75b7a8f7f890050a8dd
Kevin Ostafinski <kevinostafinski@gmail.com>
2020-05-20 15:20:52 +0200

    defined default types

--- typeDefs.js
+++ typeDefs.js
@@ -1,7 +1,57 @@
 const { gql } = require('apollo-server-express')
 
 module.exports = gql`
+  type UserPlayerRating {
+    playerId: String!
+    rating: Float!
+  }
+
+  type UserMatchVote {
+    matchId: String!
+    ratings: [UserPlayerRating]!
+  }
+
+  type User {
+    id: String!
+    email: String!
+    password: String!
+    roles: [String!]!
+    votes: [UserMatchVote]!
+  }
+
+  type PlayerPlayedMatch {
+    matchId: String!
+    average: Float!
+  }
+
+  type Player {
+    id: String!
+    firstName: String!
+    lastName: String!
+    played: [PlayerPlayedMatch]!
+  }
+
+  type MatchPlayerRating {
+    userId: String!
+    rating: Float!
+  }
+
+  type MatchLineupPlayer {
+    playerId: String!
+    ratings: [MatchPlayerRating]!
+  }
+
+  type Match {
+    id: String!
+    date: String!
+    opponent: String!
+    location: String!
+    lineup: [MatchLineupPlayer]!
+  }
+
   type Query {
     hello: String
   }
+
+  type Mutation {}
 `
