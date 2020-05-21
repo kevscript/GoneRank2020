18691bc8367909ebc263fdc9ffec91b9fc7fd26f
Kevin Ostafinski <kevinostafinski@gmail.com>
2020-05-21 13:46:12 +0200

    added client queries for authentication

---
+++ client/src/graphql/queries/auth.js
@@ -0,0 +1,21 @@
+import { gql } from 'apollo-boost'
+
+export const LOGIN_USER = gql`
+  mutation LoginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      userId
+      token
+      tokenExpiration
+      roles
+    }
+  }
+`
+
+export const REGISTER_USER = gql`
+  mutation RegisterUser($email: String!, $password: String!) {
+    register(email: $email, password: $password) {
+      _id
+      email
+    }
+  }
+`
