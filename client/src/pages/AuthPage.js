7cbddb27ac627e4e77139e1bdd3720495df944ee
Kevin Ostafinski <kevinostafinski@gmail.com>
2020-05-21 13:47:40 +0200

    connected forms to AuthPage

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import LoginForm from '../components/forms/LoginForm'
+import RegisterForm from '../components/forms/RegisterForm'
 
-const AuthPage = () => {
-  return <div>AuthPage</div>
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+`
+
+const AuthPage = ({ handleUser }) => {
+  const [isLogin, setIsLogin] = useState(true)
+  return (
+    <Container>
+      {isLogin ? (
+        <LoginForm setIsLogin={setIsLogin} handleUser={handleUser} />
+      ) : (
+        <RegisterForm setIsLogin={setIsLogin} />
+      )}
+    </Container>
+  )
 }
 
 export default AuthPage
