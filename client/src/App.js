c33a01d9f11281cf507a70e0da1a565995993ceb
Kevin Ostafinski <kevinostafinski@gmail.com>
2020-05-21 13:23:11 +0200

    setup default routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,32 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+import PrivateRoute from './routes/PrivateRoute'
+import AdminRoute from './routes/AdminRoute'
+import GlobalStyle from './styles/global'
 
 const App = () => {
-  return <div>App</div>
+  const [user, setUser] = useState({ id: null, token: null, roles: [] })
+  const handleUser = (id, token, roles) =>
+    setUser({ id: id, token: token, roles: roles })
+
+  return (
+    <div>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Switch>
+          <PrivateRoute exact path="/" user={user} component={HomePage} />
+          <AdminRoute path="/admin" user={user} component={AdminPage} />
+          <Route path="/authentication">
+            {user.token && user.id ? (
+              <Redirect to="/" />
+            ) : (
+              <AuthPage handleUser={handleUser} />
+            )}
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
