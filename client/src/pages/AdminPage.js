3a54b40960e9242b4ec13d75ee21905126ac01d9
Kevin Ostafinski <kevinostafinski@gmail.com>
2020-05-22 16:25:12 +0200

    create Admin Page routes + added player creation in Players Page

--- client/src/pages/AdminPage.js
+++ client/src/pages/AdminPage.js
@@ -1,7 +1,47 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Route, Switch, Link } from 'react-router-dom'
+import AdminRoute from '../routes/AdminRoute'
+import PlayersPage from './PlayersPage'
+import MatchesPage from './MatchesPage'
 
-const AdminPage = () => {
-  return <div>AdminPage</div>
+const Container = styled.div``
+
+const Header = styled.div`
+  width: 100%;
+  height: 80px;
+  background: rgba(20, 56, 127, 1);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: rgba(255, 255, 255, 1);
+`
+
+const HeaderLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+  font-size: 1.25rem;
+  font-weight: 600;
+  margin: 0 1.5rem;
+
+  &:hover {
+    color: red;
+  }
+`
+
+const AdminPage = ({ user }) => {
+  return (
+    <Container>
+      <Header>
+        <HeaderLink to="/admin/players">Players</HeaderLink>
+        <HeaderLink to="/admin/matches">Matches</HeaderLink>
+      </Header>
+      <Switch>
+        <AdminRoute path="/admin/players" user={user} component={PlayersPage} />
+        <AdminRoute path="/admin/matches" user={user} component={MatchesPage} />
+      </Switch>
+    </Container>
+  )
 }
 
 export default AdminPage
---
+++ client/src/pages/MatchesPage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const MatchesPage = () => {
+  return <div>Matches</div>
+}
+
+export default MatchesPage
