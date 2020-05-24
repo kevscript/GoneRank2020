0433066bd120d21ea81063034bd65f3f289a185b
Kevin Ostafinski <kevinostafinski@gmail.com>
2020-05-24 20:41:21 +0200

    added CREATE_MATCH client query

--- client/src/graphql/queries/match.js
+++ client/src/graphql/queries/match.js
@@ -49,3 +49,38 @@ export const GET_MATCH = gql`
     }
   }
 `
+
+export const CREATE_MATCH = gql`
+  mutation CreateMatch(
+    $date: String!
+    $opponent: String!
+    $location: String!
+    $playerIds: [String!]!
+  ) {
+    createMatch(
+      date: $date
+      opponent: $opponent
+      location: $location
+      playerIds: $playerIds
+    ) {
+      id
+      date
+      opponent
+      location
+      lineup {
+        playerId
+        ratings {
+          userId
+          rating
+        }
+        average
+        infos {
+          firstName
+          lastName
+          globalAverage
+        }
+      }
+      active
+    }
+  }
+`
