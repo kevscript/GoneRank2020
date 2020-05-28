ecda8522a4395144eea2032fbf96b20d10f9300b
Kevin Ostafinski <kevinostafinski@gmail.com>
2020-05-28 22:28:04 +0200

    fixed sortByAvg - needed to reverse the sorting order

--- client/src/utils/sortByAvg.js
+++ client/src/utils/sortByAvg.js
@@ -2,7 +2,7 @@ export const sortByAvg = (players) => {
   const sortedPlayers = players.sort((a, b) => {
     const avgA = a.globalAverage
     const avgB = b.globalAverage
-    return avgA > avgB ? 1 : avgA < avgB ? -1 : 0
+    return avgA > avgB ? -1 : avgA < avgB ? 1 : 0
   })
   return sortedPlayers
 }
