9597130aec691e06729dc715b2e89d427cd3efed
Kevin Ostafinski <kevinostafinski@gmail.com>
2020-05-28 22:18:01 +0200

    fixed globalAverage field resolver, now returns 0 when there is no avg to calculate

--- resolvers/player.js
+++ resolvers/player.js
@@ -5,64 +5,98 @@ const Match = require('../models/match')
 module.exports = {
   Query: {
     players: async (_, __, req) => {
-      if (!req.isAuth) { throw new ApolloError('Unauthorized Request') }
+      if (!req.isAuth) {
+        throw new ApolloError('Unauthorized Request')
+      }
       try {
         const players = await Player.find()
         return players
-      } catch (err) { throw new ApolloError(err) }
+      } catch (err) {
+        throw new ApolloError(err)
+      }
     },
     player: async (_, { id }, req) => {
-      if (!req.isAuth) { throw new ApolloError('Unauthorized Request') }
+      if (!req.isAuth) {
+        throw new ApolloError('Unauthorized Request')
+      }
       try {
         const player = await Player.findOne({ _id: id })
-        if (!player) { throw new ApolloError(`Player with id ${id} doesn't exist in the DB.`)}
+        if (!player) {
+          throw new ApolloError(`Player with id ${id} doesn't exist in the DB.`)
+        }
         return player
-      } catch (err) { throw new ApolloError(err) }
-    }
+      } catch (err) {
+        throw new ApolloError(err)
+      }
+    },
   },
   Mutation: {
     addPlayer: async (_, { firstName, lastName }, req) => {
-      if (!req.isAuth || !req.userRoles.includes('ADMIN')) { 
+      if (!req.isAuth || !req.userRoles.includes('ADMIN')) {
         throw new ApolloError('Unauthorized Request')
       }
       try {
         // check if player with this name already exists, throw an error if true
         const exists = await Player.findOne({ firstName, lastName })
-        if (exists) { throw new UserInputError(`${firstName} ${lastName} already exists in the DB.`) }
+        if (exists) {
+          throw new UserInputError(
+            `${firstName} ${lastName} already exists in the DB.`
+          )
+        }
         const newPlayer = { firstName, lastName, matchesPlayed: [] }
         const player = await new Player(newPlayer).save()
         return player
-      } catch (err) { throw new ApolloError(err) }
+      } catch (err) {
+        throw new ApolloError(err)
+      }
     },
     removePlayer: async (_, { id }, req) => {
-      if (!req.isAuth || !req.userRoles.includes('ADMIN')) { 
+      if (!req.isAuth || !req.userRoles.includes('ADMIN')) {
         throw new ApolloError('Unauthorized Request')
       }
       try {
         const player = await Player.findOneAndDelete({ _id: id })
-        if (!player) { throw new UserInputError(`Player with id ${id} doesn't exist.`)}
+        if (!player) {
+          throw new UserInputError(`Player with id ${id} doesn't exist.`)
+        }
         return player
-      } catch (err) { throw new ApolloError(err) }
-    }
+      } catch (err) {
+        throw new ApolloError(err)
+      }
+    },
   },
   Player: {
     globalAverage: async (player) => {
       if (player.matchesPlayed.length < 1) {
         return 0
-      } else {
-        const matchPromises = player.matchesPlayed.map(m => Match.findOne({ id: m.matchId }))
+      }
+      try {
+        const matchPromises = player.matchesPlayed.map((m) =>
+          Match.findOne({ id: m.matchId })
+        )
         const matches = await Promise.all(matchPromises)
-        const averages = matches.map(m => {
-          const him = m.lineup.find(p => p.playerId === player.id)
+        const averages = matches.map((m) => {
+          const him = m.lineup.find((p) => p.playerId === player.id)
           if (him.ratings.length > 0) {
             const sum = him.ratings.reduce((acc, curr) => acc + curr.rating, 0)
-            return parseFloat((sum / him.ratings.length))
-          } else { return NaN }
+            return parseFloat(sum / him.ratings.length)
+          } else {
+            return 0
+          }
         })
-        const filteredAvgs = averages.filter(avg => Number.isNaN(avg) === false)
-        const globalAvg = filteredAvgs.reduce((acc, curr) => acc + curr, 0) / filteredAvgs.length
+        const filteredAvgs = averages.filter(
+          (avg) => Number.isNaN(avg) === false
+        )
+        if (filteredAvgs.length === 0) {
+          return 0
+        }
+        const globalAvg =
+          filteredAvgs.reduce((acc, curr) => acc + curr, 0) /
+          filteredAvgs.length
         return parseFloat(globalAvg.toFixed(2))
+      } catch (err) {
+        throw new ApolloError(err)
       }
-    }
-  }
+    },
+  },
 }
