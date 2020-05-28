25f98fb7ad186be97976acd4eca8173c1c892544
Kevin Ostafinski <kevinostafinski@gmail.com>
2020-05-28 17:22:37 +0200

    updated Routes logic + created handleUser

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { BrowserRouter, Route, Switch, Redirect, Link } from 'react-router-dom'
 import PrivateRoute from './routes/PrivateRoute'
 import HomePage from './pages/HomePage'
@@ -9,30 +9,44 @@ import AuthPage from './pages/AuthPage'
 const App = () => {
   const [user, setUser] = useState({ id: null, token: null, roles: [] })
 
+  // useEffect(() => {
+  //   const id = localStorage.getItem('userId')
+  //   const token = localStorage.getItem('token')
+  //   const roles = JSON.parse(localStorage.getItem('userRoles'))
+  //   setUser({ id, token, roles })
+  // }, [])
+
+  const handleUser = ({ id, token, roles }) => {
+    setUser({ id: id, token: token, roles: roles })
+    localStorage.setItem('token', token)
+    localStorage.setItem('userId', id)
+    localStorage.setItem('userRoles', JSON.stringify(roles))
+  }
+
   return (
     <div>
       <BrowserRouter>
         <Switch>
-          <Route exact path="/">
-            <HomePage user={user} />
-          </Route>
-          <Route path="/authentication">
-            <AuthPage user={user} />
-          </Route>
-          <PrivateRoute path="/matchs" user={user} component={<MatchsPage />} />
+          <PrivateRoute exact path="/" user={user} component={HomePage} />
+          {!user.id && !user.token && (
+            <Route path="/authentication">
+              <AuthPage user={user} handleUser={handleUser} />
+            </Route>
+          )}
+          <PrivateRoute path="/matchs" user={user} component={MatchsPage} />
           <PrivateRoute
             path="/match/id/:matchId"
             user={user}
-            component={<MatchPage />}
+            component={MatchPage}
           />
           <Route render={() => <Redirect to={{ pathname: '/' }} />} />
         </Switch>
-        <div>
+        {/* <div>
           <Link to="/">Home</Link>
           <Link to="/authentication">Login</Link>
           <Link to="/matchs">Matchs</Link>
           <Link to="/match/:matchId">Match</Link>
-        </div>
+        </div> */}
       </BrowserRouter>
     </div>
   )
