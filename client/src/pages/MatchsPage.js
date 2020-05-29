2128bca98ca35337bbc0e14f3f5a8c03be4dde78
Kevin Ostafinski <kevinostafinski@gmail.com>
2020-05-29 15:01:04 +0200

    created MatchsPage (complete)

--- client/src/pages/MatchsPage.js
+++ client/src/pages/MatchsPage.js
@@ -1,7 +1,90 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import { useQuery } from '@apollo/react-hooks'
+import { GET_MATCHES } from '../graphql/queries/match'
+import { sortMatchesByDate } from '../utils/sortMatchesByDate'
+
+const Container = styled.div`
+  width: 100%;
+`
+
+const MatchsList = styled.div`
+  margin: 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const MatchItem = styled(Link)`
+  text-decoration: none;
+  display: flex;
+  width: 100%;
+  height: 50px;
+  background: #f5f5f5;
+  margin-bottom: 5px;
+`
+
+const MatchInfo = styled.div`
+  background: #dbdbdb;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  color: #14387f;
+  width: 50px;
+`
+
+const MatchData = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 1rem;
+`
+
+const MatchLocation = styled.div`
+  font-size: 10px;
+`
+const MatchDate = styled.span`
+  font-size: 10px;
+`
+const MatchOpponent = styled.span`
+  color: #14387f;
+`
+
+const MatchRating = styled.span`
+  color: #14387f;
+  font-weight: 600;
+`
 
 const MatchsPage = () => {
-  return <div>Matchs Page</div>
+  const { loading, error, data: { matches } = {} } = useQuery(GET_MATCHES, {
+    onCompleted: (res) => console.log(res),
+  })
+  return (
+    <Container>
+      <MatchsList>
+        {matches &&
+          sortMatchesByDate(matches).map((match) => (
+            <MatchItem to={`/home/matches/id/${match.id}`} key={match.id}>
+              <MatchInfo>
+                <MatchLocation>
+                  {match.location === 'home' ? 'H' : 'A'}
+                </MatchLocation>
+                <MatchDate>{match.date.slice(0, 5)}</MatchDate>
+              </MatchInfo>
+              <MatchData>
+                <MatchOpponent>{match.opponent}</MatchOpponent>
+                <MatchRating>5</MatchRating>
+              </MatchData>
+            </MatchItem>
+          ))}
+      </MatchsList>
+    </Container>
+  )
 }
 
 export default MatchsPage
