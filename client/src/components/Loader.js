fb79f698384e8d6590f037e83b6791ea9ca570c8
Kevin Ostafinski <kevinostafinski@gmail.com>
2020-05-31 21:21:04 +0200

    created loader component

---
+++ client/src/components/Loader.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+import MoonLoader from 'react-spinners/MoonLoader'
+
+const LoadingContainer = styled.div`
+  width: 100%;
+  height: calc(100vh - 80px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Loader = ({ size = 100 }) => {
+  return (
+    <LoadingContainer>
+      <MoonLoader color={'#14387f'} size={size} />
+    </LoadingContainer>
+  )
+}
+
+export default Loader
