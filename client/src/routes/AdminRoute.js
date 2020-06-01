533d7140ffea4436ae1089f6d77181013bf47e61
Kevin Ostafinski <kevinostafinski@gmail.com>
2020-06-01 13:34:43 +0200

    created AdminRoute component

---
+++ client/src/routes/AdminRoute.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+
+const AdminRoute = ({ component: Component, user, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        user.roles.includes('ADMIN') ? (
+          <Component user={user} {...rest} {...props} />
+        ) : (
+          <Redirect to="/" />
+        )
+      }
+    />
+  )
+}
+
+export default AdminRoute
