87a11d053829aa59ccb1156621f47d6812b9d9e5
Kevin Ostafinski <kevinostafinski@gmail.com>
2020-07-13 17:06:15 +0200

    added sorting argument <ASC|DESC> to sortMatchesByDate util function

--- client/src/utils/sortMatchesByDate.js
+++ client/src/utils/sortMatchesByDate.js
@@ -1,7 +1,10 @@
-export const sortMatchesByDate = (matches) => {
+export const sortMatchesByDate = (matches, sort = 'ASC') => {
   const sortedMatches = matches.sort((a, b) => {
     const dateA = a.date.split('/').reverse().join('')
     const dateB = b.date.split('/').reverse().join('')
+    if (sort === 'DESC') {
+      return dateA > dateB ? -1 : dateA < dateB ? 1 : 0
+    }
     return dateA > dateB ? 1 : dateA < dateB ? -1 : 0
   })
   return sortedMatches
